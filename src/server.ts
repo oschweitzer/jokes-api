58467f9cdebafa9380576606a7ff053be3e05060
Olivier Schweitzer <olivier.sch68@gmail.com>
2018-02-10 16:42:08 +0100

    feat: Add cors

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
 import {INestApplication} from '@nestjs/common/interfaces/nest-application.interface';
 import * as woodcutter from 'woodcutter';
+import * as cors from 'cors';
+import {ValidationPipe} from "@nestjs/common";
 
 const app: Promise<INestApplication> = NestFactory.create(ApplicationModule);
 app.then(instance => {
-  const logger = new woodcutter.WoodCutter();
+      const logger = new woodcutter.WoodCutter();
+      instance.use(cors());
       instance.listen(3000, () =>
-         logger.info('Application is listening on port 3000')
+          logger.info('Application is listening on port 3000')
       )
     }
 );
