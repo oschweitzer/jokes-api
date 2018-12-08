a2c3768786845de0994ed1577f64c452f004dc02
Oli1810 <olivier.sch68@gmail.com>
2018-12-08 13:41:16 +0100

    feat(server): Add env variable for the HTTP port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   dadJokesApi: process.env.DAD_JOKES_API || 'https://icanhazdadjoke.com',
-  chuckNorrisJokesApi: process.env.CHUCK_JOKES_API || 'https://api.chucknorris.io/jokes/random'
+  chuckNorrisJokesApi: process.env.CHUCK_JOKES_API || 'https://api.chucknorris.io/jokes/random',
+  port:  process.env.hasOwnProperty('HTTP_PORT') ? process.env['HTTP_PORT'] : 3000
 };
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import {INestApplication} from '@nestjs/common/interfaces/nest-application.inter
 import * as woodcutter from 'woodcutter';
 import * as cors from 'cors';
 import {ValidationPipe} from "@nestjs/common";
+import {environment} from './environments/environment';
 
 const app: Promise<INestApplication> = NestFactory.create(ApplicationModule);
 app.then(instance => {
       const logger = new woodcutter.WoodCutter();
       instance.enableCors();
-      instance.listen(3000, () =>
-          logger.info('Application is listening on port 3000')
+      instance.listen(environment.port, () =>
+          logger.info(`Application is listening on port ${environment.port}`)
       )
     }
 );
