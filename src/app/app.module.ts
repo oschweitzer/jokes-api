ff6d7f57d117dbbb59e91de3b27da297fbc16fac
Oli1810 <olivier.sch68@gmail.com>
2018-12-08 13:39:09 +0100

    refactor(version): Delete version module since nodegit won't work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import {Module} from '@nestjs/common';
 import {JokesModule} from "./modules/jokes/jokes.module";
-import {VersionModule} from "./modules/version/version.module";
 
 @Module({
   modules: [
       JokesModule,
-      VersionModule
   ]
 })
 export class ApplicationModule {}
